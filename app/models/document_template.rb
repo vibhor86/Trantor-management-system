621a402ee7bf089a87e57ce0091895020516aacd
Aastha <aastha.tangri@trantorinc.com>
2012-08-17 10:46:54 +0530

    -a

--- app/models/document_template.rb
+++
@@ -1,5 +0,0 @@
-class DocumentTemplate < ActiveRecord::Base
-  attr_accessible :body, :title
-  validates :title, :body, :presence => true
-  validates :title, :uniqueness => true
-end
--- spec/factories/placeholders.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :placeholder do
-  column_name "column"
-  placeholder_name "|{placeholder}|"
-  
-  end
-end
