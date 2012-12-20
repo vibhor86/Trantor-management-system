a7dc0b5d7fd1cc2a77fd3203315e96c4b8441e9c
arun <trustarun@yahoo.co.in>
2012-12-20 11:40:12 +0530

    registration separated from devise and implemented independently

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Tms::Application.routes.draw do
   root :to => "admin/dashboards#index"
   namespace :admin do
     resources :dashboards, :only => :index
+    resources :employee_enrollments
   end
 end
