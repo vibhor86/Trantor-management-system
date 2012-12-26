d38e50356a47b64782c98a58b867a08593d7b71b
mahinder <mahinder124@gmail.com>
2012-12-26 10:10:50 -0800

    add audit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base 
+audited
   devise :database_authenticatable, 
     :recoverable, :rememberable, :trackable, :validatable
   attr_accessible :email, :password, :password_confirmation, :remember_me ,:ecode
