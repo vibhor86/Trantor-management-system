0daf046794f2024b43f4b9ca2d94494f6e8c29f4
mahinder <mahinder.kumar@trantorinc.com>
2013-03-01 15:34:55 +0530

    update

---
+++ app/assets/javascripts/leave.js
@@ -0,0 +1,14 @@
+$(function(){
+    
+    //   $('#type_leave_id').on('click',function(){
+    //       type_id = $(this).val()
+    //       
+    //     $.get('/leavemanagements/total_balance',{'leave_type_id' : type_id},function(data){
+    //        $('#balance_change').html(data);
+    //    });  
+    //   }); 
+
+    
+    
+    
+})
