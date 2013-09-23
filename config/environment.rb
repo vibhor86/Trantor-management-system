0eb5d0131187e731929f0c89828df2292ce89b8a
Moin Haidar <moinhaidar@gmail.com>
2013-09-23 12:19:54 +0530

    heroku h13 error fix

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TrantorManagementSystem::Application.initialize!
+
+# Heroku H13 Fix
+if Rack::Utils.respond_to?("key_space_limit=")
+  Rack::Utils.key_space_limit = 262144 # 4 times the default size
+end
